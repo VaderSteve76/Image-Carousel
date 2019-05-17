1887946dd6762e0b345ca542efca5875abfae136
VaderSteve76 <alverson.s@yahoo.com>
2019-05-17 19:18:07 -0400

    Transitions working

--- carousel.js
+++ carousel.js
@@ -19,3 +19,18 @@ prevBtn.addEventListener('click', () => {
   counter--;
   carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
+
+carouselSlide.addEventListener('transitionend', () => {
+
+  if(carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length -2;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+
+  if(carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
